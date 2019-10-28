be82d296241a4c0d66be245e6350f5f0595c215b
Petro Vorotyntsev <biowarius@gmail.com>
2019-10-28 16:41:48 +0200

    Formatting code by 'Black'

--- Lesson_4/bot_for_medium.py
+++ Lesson_4/bot_for_medium.py
@@ -8,33 +8,38 @@ from time import sleep
 from bs4 import BeautifulSoup
 import utils
 
-URL_MEDIUM = 'https://medium.com/topic/technology'
-BOT_TOKEN = 'YOUR_TOKEN_HERE'
-CHANNEL_NAME = '@CHANNEL'
+URL_MEDIUM = "https://medium.com/topic/technology"
+BOT_TOKEN = "YOUR_TOKEN_HERE"
+CHANNEL_NAME = "@CHANNEL"
 
 SINGLE_RUN = False
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 def getLinks(url):
     html_page = requests.get(url).text
     soup = BeautifulSoup(html_page)
     links = set()
-    for i in soup.find_all('a', attrs={'href': re.compile("^https://")}):
-        link = i.get('href')
-        if 'https://medium.com/m/signin?operation=register' not in link and \
-           'https://towardsdatascience.com/?source' not in link and \
-           'https://medium.com/m/signin?' not in link and \
-           'https://medium.com/membership?s' not in link:
+    for i in soup.find_all("a", attrs={"href": re.compile("^https://")}):
+        link = i.get("href")
+        if (
+            "https://medium.com/m/signin?operation=register" not in link
+            and "https://towardsdatascience.com/?source" not in link
+            and "https://medium.com/m/signin?" not in link
+            and "https://medium.com/membership?s" not in link
+        ):
             links.add(link)
 
     return list(links)
-        
+
+
 def append_table():
     links = getLinks(URL_MEDIUM)
-    content = utils.SQLighter('ArticleMedium.db')
+    content = utils.SQLighter("ArticleMedium.db")
     for link in links:
-        content.create_article(link)    
+        content.create_article(link)
+
 
 def send_new_post(item, connection):
     print(item)
@@ -43,30 +48,31 @@ def send_new_post(item, connection):
     connection.change_status(item[0], utils.USED)
     sleep(1)
     return
-        
-if __name__=='__main__':
-    logging.getLogger('requests').setLevel(logging.CRITICAL)
-    logging.basicConfig(level=logging.INFO,
-                        filename='bot_log.log')
-    
-    content = utils.SQLighter('ArticleMedium.db')
+
+
+if __name__ == "__main__":
+    logging.getLogger("requests").setLevel(logging.CRITICAL)
+    logging.basicConfig(level=logging.INFO, filename="bot_log.log")
+
+    content = utils.SQLighter("ArticleMedium.db")
     content.set_all_to_unused()
-    
+
     while True:
         article = content.select_last_unused()
         send_new_post(article, content)
         sleep(10)
-        
+
         try:
             if content.select_last_unused()[0] % 3 == 0:
-                bot.send_message(CHANNEL_NAME, "We will be back in a Minute ;)")    
-                sleep(60)       
+                bot.send_message(CHANNEL_NAME, "We will be back in a Minute ;)")
+                sleep(60)
         except Exception as error:
-            bot.send_message(CHANNEL_NAME, "Looks like we are out of articles for today. Come here tomorrow pal :)")    
+            bot.send_message(
+                CHANNEL_NAME,
+                "Looks like we are out of articles for today. Come here tomorrow pal :)",
+            )
             append_table()
             sleep(30)
-            
+
         used_articles = content.select_all_used()
         all_articles = content.select_all()
-        
-            
--- Lesson_4/utils.py
+++ Lesson_4/utils.py
@@ -4,24 +4,28 @@ import logging
 USED = 1
 UNUSED = 0
 
+
 class SQLighter:
     def __init__(self, database):
         try:
             self.connection = sqlite3.connect(database)
         except Exception as err:
-            print(f'[Error] {err}.')
+            print(f"[Error] {err}.")
             exit(1)
         self.cursor = self.connection.cursor()
-        
+
     def create_article(self, article):
-        sql = '''INSERT INTO ARTICLES(Article_Name, Used_in_channel)
-                 VALUES(?,?)'''
-        all_articles = [article[1] for article in self.cursor.execute("SELECT * FROM ARTICLES").fetchall()]
+        sql = """INSERT INTO ARTICLES(Article_Name, Used_in_channel)
+                 VALUES(?,?)"""
+        all_articles = [
+            article[1]
+            for article in self.cursor.execute("SELECT * FROM ARTICLES").fetchall()
+        ]
         if article in all_articles:
-            logging.info('[INFO] Article already exist: %s' % (article))
+            logging.info("[INFO] Article already exist: %s" % (article))
             return
         else:
-            logging.info('[INFO] Article created: %s' % (article))
+            logging.info("[INFO] Article created: %s" % (article))
             self.cursor.execute(sql, (article, False))
             self.connection.commit()
             return self.cursor.lastrowid
@@ -35,16 +39,17 @@ class SQLighter:
         """Get last row"""
         last = len(self.select_all())
         with self.connection:
-            return self.cursor.execute("SELECT * FROM ARTICLES WHERE generated_id = ?",
-                                       (last,)).fetchall()
+            return self.cursor.execute(
+                "SELECT * FROM ARTICLES WHERE generated_id = ?", (last,)
+            ).fetchall()
 
     def select_last_unused(self):
         """Get last unused row"""
         all_articles = self.cursor.execute("SELECT * FROM ARTICLES").fetchall()
         for article in all_articles:
             if article[2] == 0:
-                return article 
-        
+                return article
+
     def select_all_used(self):
         """Get all used row"""
         all_articles = self.cursor.execute("SELECT * FROM ARTICLES").fetchall()
@@ -53,21 +58,21 @@ class SQLighter:
             if article[2] == 1:
                 used_articles.append(article)
         return used_articles
-        
+
     def set_all_to_unused(self):
-        sql = '''UPDATE ARTICLES SET Used_in_channel = 0'''
+        sql = """UPDATE ARTICLES SET Used_in_channel = 0"""
         self.cursor.execute(sql)
         self.connection.commit()
-        return 
-    
+        return
+
     def change_status(self, row, status):
         """Change status to used"""
-        sql = '''UPDATE ARTICLES SET Used_in_channel = ? WHERE generated_id = ?'''
-        self.cursor.execute(sql, (status, row,))
+        sql = """UPDATE ARTICLES SET Used_in_channel = ? WHERE generated_id = ?"""
+        self.cursor.execute(sql, (status, row))
         self.connection.commit()
-        logging.info('[INFO] Status of article changed: %s' % (self.select_single(row)))
-        return 
-    
+        logging.info("[INFO] Status of article changed: %s" % (self.select_single(row)))
+        return
+
     def select_single(self, rownum):
         """Get one row by rownum"""
         with self.connection:
@@ -84,5 +89,4 @@ class SQLighter:
     def close(self):
         """Close db connection"""
         self.connection.close()
-        logging.info('[INFO] Conection closed.')
-        
+        logging.info("[INFO] Conection closed.")
